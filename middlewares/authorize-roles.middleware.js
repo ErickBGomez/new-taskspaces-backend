e12f30a86a9339c2bac36d5007e8885eeb26ce85
ErickBGomez <erickbgomez.dev@gmail.com>
2025-04-14 11:54:19 -0600

    Convert authorize roles middleware responses into ErrorResponse instances

--- middlewares/authorize-roles.middleware.js
+++ middlewares/authorize-roles.middleware.js
@@ -1,3 +1,5 @@
+import ErrorResponseBuilder from '../helpers/error-response-builder.js';
+
 export const ROLES = {
   USER: 'USER',
   SYSADMIN: 'SYSADMIN',
@@ -13,29 +15,27 @@ export const authorizeRoles = (requiredRole) => {
     const { role } = req.user;
 
     if (!req.user)
-      return res.status(401).json({
-        status: 401,
-        message: 'Unauthorized',
-        error: 'User not authenticated',
-      });
+      return ErrorResponseBuilder.setStatus(401)
+        .setMessage('Unauthorized')
+        .setError('User not authenticated')
+        .build();
 
     // ROLES[undefined] returns undefined. This happens when a role is not provided, or is anything besides USER or SYSADMIN
     if (!ROLES[role])
-      return res.status(403).json({
-        status: 403,
-        message: 'Forbidden',
-        error: 'Role invalid or not provided',
-      });
+      return ErrorResponseBuilder.setStatus(403)
+        .setMessage('Forbidden')
+        .setError('Role invalid or not provided')
+        .build();
 
     // If user has a role below the required role, deny access (this can be done by checking the hierarchy)
     // For example, undefined (role not provided or invalid) < USER < SYSADMIN
     if (ROLES_HIERARCHY[role] < ROLES_HIERARCHY[requiredRole])
-      return res.status(403).json({
-        status: 403,
-        message: 'Forbidden',
-        error:
-          'User does not have sufficient privileges to perform this action',
-      });
+      return ErrorResponseBuilder.setStatus(403)
+        .setMessage('Forbidden')
+        .setError(
+          'User does not have sufficient privileges to perform this action'
+        )
+        .build();
 
     // User has the required role (SYSADMIN can bypass all roles)
     next();
