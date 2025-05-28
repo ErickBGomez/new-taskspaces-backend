9ca053d73a3e44b4217ff9691ca29a1505d6f250
ErickBGomez <erickbgomez.dev@gmail.com>
2025-05-28 14:42:54 -0600

    Avoid returning anything if query is empty

--- services/search.service.js
+++ services/search.service.js
@@ -1,21 +1,31 @@
 import * as searchRepository from '../repositories/search.repository.js';
 
 export const searchAll = async (query, isAdmin, userId) => {
+  if (!query) return [];
+
   return await searchRepository.searchAll(query, isAdmin, userId);
 };
 
 export const searchWorkspaces = async (query, isAdmin, userId) => {
+  if (!query) return [];
+
   return await searchRepository.searchWorkspaces(query, isAdmin, userId);
 };
 
 export const searchProjects = async (query, isAdmin, userId) => {
+  if (!query) return [];
+
   return await searchRepository.searchProjects(query, isAdmin, userId);
 };
 
 export const searchTasks = async (query, isAdmin, userId) => {
+  if (!query) return [];
+
   return await searchRepository.searchTasks(query, isAdmin, userId);
 };
 
 export const searchUsers = async (query) => {
+  if (!query) return [];
+
   return await searchRepository.searchUsers(query);
 };
